b0da259e08df497021fafde8de588a182cc06972
Solaf Alessa <solaff.alessa@gmail.com>
2026-07-31 02:21:42 +0300

    Add webpack configuration for TypeScript project

---
+++ Typescript/Task_0/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/main.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    static: "./dist"
+  }
+};
